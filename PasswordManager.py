148ae7078048fb6e163b4a5ee27d5cdb234f6cbd
Alec Borque <47423277+AlecDerinBorque@users.noreply.github.com>
2025-02-11 11:13:41 -0500

    Update PasswordManager.py

--- PasswordManager.py
+++ PasswordManager.py
@@ -21,7 +21,6 @@ while True:
     mode = input("Would you like to add a new password or view existing ones (view/add), press q to quit? ").lower()
     if mode == "q":
         break
-
     elif mode == "view":
         view()
     elif mode == "add":
